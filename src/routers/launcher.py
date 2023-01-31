b3edf796902668be7781060912fd4b17e2d3baaf
Nikolay Chernikov <mukadehiganbana@gmail.com>
2023-01-31 18:12:10 +0300

    feat(unassigned): create unassugned router and method (TT-2)

---
+++ src/routers/launcher.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+from methods.JIRA import JiraManager
+
+
+router = APIRouter()
+
+VERSION = "v1"
+TAG = "Termination"
+
+
+@router.post("/" + VERSION + "/upload", tags=[TAG])
+def unassigned(user_id: int):
+    jira = JiraManager()
+    jira.unassigned(user_id)
+    return {'result': 'All tickets unassigned'}
